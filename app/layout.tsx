cb644a94ece2c62464dcd573ef4ace7818a86906
Aakash <aakash19here@gmail.com>
2025-01-04 00:26:13 +0530

    chore: add tanstack provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/lib/providers";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} antialiased`}>
+        <Providers>
+          {children}
+
+          <Toaster richColors position="top-center" />
+        </Providers>
+      </body>
     </html>
   );
 }
