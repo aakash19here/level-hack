7fbb95cea918d68844a03fbade9db7253c0d553c
Aakash <aakash19here@gmail.com>
2025-01-04 00:39:03 +0530

    feat: show messages on ui

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,19 +1,25 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { motion } from "framer-motion";
 import { SendIcon } from "lucide-react";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
 import { Icons } from "./icons";
+import { Message } from "./message";
+import { useScrollToBottom } from "./use-scroll-to-bottom";
 
 const suggestedQuestions = [
   {
     title: "What should I post",
     label: "on my social media?",
-    action: "what's the summary of these documents?",
+    action: "What should I post on my social media?",
   },
   {
     title: "What time should I",
     label: "post on my social media?",
-    action: "who is the author of these documents?",
+    action: "What time should I post on my social media?",
   },
 ];
 
@@ -35,49 +41,128 @@ const socials = [
   },
 ];
 
+type Message = {
+  role: "user" | "assistant";
+  content: string;
+};
+
 export default function Chat() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const [messagesContainerRef, messagesEndRef] =
+    useScrollToBottom<HTMLDivElement>();
+  const { mutate, error } = useMutation({
+    mutationFn: async (message: string) => {
+      const { data } = await axios.post<{ message: string }>("/api/chat", {
+        message,
+      });
+
+      return data.message;
+    },
+    onSuccess: (data) => {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        {
+          role: "assistant",
+          content: data,
+        },
+      ]);
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+      } else {
+        toast.error("Something went wrong");
+      }
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const message = formData.get("message") as string;
+    formRef.current?.reset();
+
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        role: "user",
+        content: message,
+      },
+    ]);
+
+    mutate(message);
+  };
+
   return (
     <div className="flex flex-row justify-center pb-20 h-dvh bg-white dark:bg-zinc-900">
       <div className="flex flex-col justify-between items-center gap-4">
         <div
-          // ref={messagesContainerRef}
+          ref={messagesContainerRef}
           className="flex flex-col gap-4 h-full w-dvw items-center overflow-y-scroll"
         >
+          {messages.map((message, index) => (
+            <Message
+              key={`${index}`}
+              role={message.role}
+              content={message.content}
+            />
+          ))}
           <div
-            //   ref={messagesEndRef}
+            ref={messagesEndRef}
             className="flex-shrink-0 min-w-[24px] min-h-[24px]"
           />
         </div>
 
-        <div className="grid sm:grid-cols-2 gap-2 w-full px-4 md:px-0 mx-auto md:max-w-[500px]">
-          {suggestedQuestions.map((question, index) => (
-            <motion.div
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.05 * index }}
-              key={index}
-              className={index > 1 ? "hidden sm:block" : "block"}
-            >
-              <button className="w-full text-left border border-zinc-200 dark:border-zinc-800 text-zinc-800 dark:text-zinc-300 rounded-lg p-2 text-sm hover:bg-zinc-100 dark:hover:bg-zinc-800 transition-colors flex flex-col">
-                <span className="font-medium">{question.title}</span>
-                <span className="text-zinc-500 dark:text-zinc-400">
-                  {question.label}
-                </span>
-              </button>
-            </motion.div>
-          ))}
+        <div className="grid sm:grid-cols-2 gap-2 w-full px-4 md:px-0 mx-auto md:max-w-xl">
+          {messages.length === 0 &&
+            suggestedQuestions.map((question, index) => (
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.05 * index }}
+                key={index}
+                className={index > 1 ? "hidden sm:block" : "block"}
+              >
+                <button
+                  onClick={() => {
+                    setMessages((prevMessages) => [
+                      ...prevMessages,
+                      {
+                        role: "user",
+                        content: question.action,
+                      },
+                    ]);
+                    mutate(question.action);
+                  }}
+                  className="w-full text-left border border-zinc-200 dark:border-zinc-800 text-zinc-800 dark:text-zinc-300 rounded-lg p-2 text-sm hover:bg-zinc-100 dark:hover:bg-zinc-800 transition-colors flex flex-col"
+                >
+                  <span className="font-medium">{question.title}</span>
+                  <span className="text-zinc-500 dark:text-zinc-400">
+                    {question.label}
+                  </span>
+                </button>
+              </motion.div>
+            ))}
         </div>
 
-        <form className="flex flex-row gap-2 relative items-center w-full md:max-w-[500px] max-w-[calc(100dvw-32px) px-4 md:px-0">
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="flex flex-row gap-2 relative items-center w-full md:max-w-xl max-w-[calc(100dvw-32px) px-4 md:px-0"
+        >
           <input
             className="bg-zinc-100 rounded-md px-2 py-1.5 flex-1 outline-none dark:bg-zinc-700 text-zinc-800 dark:text-zinc-300"
             placeholder="Send a message..."
+            name="message"
+            required
           />
           <button className="bg-blue-500 text-white rounded-md px-2 py-1.5">
             <SendIcon />
           </button>
         </form>
-        <div className="grid sm:grid-cols-3 gap-2 w-full px-4 md:px-0 mx-auto md:max-w-[500px]">
+        <div className="grid sm:grid-cols-3 gap-2 w-full px-4 md:px-0 mx-auto md:max-w-xl">
           {socials.map((social, index) => (
             <motion.div
               initial={{ opacity: 0, y: -20 }}
---
+++ components/message.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { BotIcon, UserIcon } from "lucide-react";
+import { ReactNode } from "react";
+import { Markdown } from "./markdown";
+import { Separator } from "./ui/separator";
+
+export const Message = ({
+  role,
+  content,
+}: {
+  role: string;
+  content: string | ReactNode;
+}) => {
+  return (
+    <motion.div
+      className={`flex flex-row gap-4 px-4 w-full max-w-xl md:px-0 first-of-type:pt-20`}
+      initial={{ y: 5, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+    >
+      <div className="size-[32px] rounded-md flex flex-col border border-accent-foreground/10 bg-secondary text-primary justify-center items-center flex-shrink-0">
+        {role === "assistant" ? <BotIcon /> : <UserIcon />}
+      </div>
+
+      <div className="flex flex-col gap-6 w-full">
+        <div className="text-zinc-800 dark:text-zinc-300 flex flex-col gap-4">
+          <Markdown>{content as string}</Markdown>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
